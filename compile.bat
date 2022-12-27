7315764f97421261f6e3a98a50cdf849ad495886
green <13384496+green-new@users.noreply.github.com>
2022-12-27 17:25:47 -0500

    Accounted for main.c move to src

--- compile.bat
+++ compile.bat
@@ -1,2 +1,2 @@
 @echo off
-gcc main.c ./src/obj.c ./src/mtl.c ./src/utils.c -o main -Iinclude -g -Wall -std=c99 -pedantic
+gcc ./src/main.c ./src/obj.c ./src/mtl.c ./src/utils.c -o main -Iinclude -g -Wall -std=c99 -pedantic
